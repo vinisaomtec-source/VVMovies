520f6a5aaa4a4fd3bbb1ad43063b62655d51870a
vinisaomtec-source <vinisaomtec@gmail.com>
2025-10-08 14:03:56 -0300

    V0.1

---
+++ VVMovie/navigation/appNavigation.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreens';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function appNavigation() {
+    return (
+        <navigatorConatiner>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name='Home'
+                    opitions={{headerShown: false}}
+                    component={{HomeScreen}} />
+            </Stack.Navigator>
+        </navigatorConatiner>
+    )
+}
