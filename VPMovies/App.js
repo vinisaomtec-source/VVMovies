9f1ba73375562daef63af4aa5e69bc978da21c68
vinisaomtec-source <vinisaomtec@gmail.com>
2025-10-22 13:56:20 -0300

    V0.3

---
+++ VPMovies/App.js
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import "./global.css";
+import AppNavigation from "./navigation/appNavigation";
+
+export default function App() {
+  return (
+    <AppNavigation />
+  );
+}
